d5ec50858006469c6b6303ab8bdffdfd887c5b7d
Jing Chun Tan <jtan1993@gmail.com>
2018-04-06 15:58:19 +0100

    Create FaceCamera.cs

---
+++ FaceCamera.cs
@@ -0,0 +1,26 @@
+//a script to allow the object(text label etc) to always face the camera perfectly
+
+using UnityEngine;
+using System.Collections;
+
+public class FaceCamera : MonoBehaviour {
+
+	public Camera m_camera;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+		if (m_camera == null)
+		{
+			m_camera = Camera.main;
+		}
+	}
+	
+	// Update is called once per frame 
+	void Update () 
+	{
+		transform.LookAt(transform.position + m_camera.transform.rotation * Vector3.forward, m_camera.transform.rotation * Vector3.up);
+
+	}
+}

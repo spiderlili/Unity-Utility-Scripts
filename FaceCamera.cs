e8aadf5556eab29f2312be0afc7d9d211feb8f96
Jing Chun Tan <jtan1993@gmail.com>
2018-04-06 16:02:09 +0100

    Update FaceCamera.cs

--- FaceCamera.cs
+++ FaceCamera.cs
@@ -1,4 +1,4 @@
-//a script to allow the object(text label etc) to always face the camera perfectly
+//a script to allow the prefab object(text label etc) to always face the camera perfectly
 
 using UnityEngine;
 using System.Collections;
@@ -10,7 +10,7 @@ public class FaceCamera : MonoBehaviour {
 	// Use this for initialization
 	void Start () 
 	{
-	
+	//find the camera automatically at the start
 		if (m_camera == null)
 		{
 			m_camera = Camera.main;
@@ -20,6 +20,9 @@ public class FaceCamera : MonoBehaviour {
 	// Update is called once per frame 
 	void Update () 
 	{
+		//forces the sprite to look in the same direction as the camera
+		//takes a target vector(forward vector of the camera added to current position) and a world up vector(up vector of the camera)
+		//make the ui text align with the camera on every frame like a billboard to camera at all times
 		transform.LookAt(transform.position + m_camera.transform.rotation * Vector3.forward, m_camera.transform.rotation * Vector3.up);
 
 	}
